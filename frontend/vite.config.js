2f33de99fbd947104eb3c89ca3416a4e86bfbed3
toorkk <torkermeco@gmail.com>
2025-07-03 16:11:52 +0200

    rerender fix 3

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,15 +4,26 @@ import tailwindcss from '@tailwindcss/vite'
 import {vitePrerenderPlugin} from 'vite-prerender-plugin';
 import path from 'path'
 
+// brute force rešitev ker vitePrerenderPlugin ne zaključi procesa, sel gledat njihov github issues in bi naj bil react bug ki se za zdaj ni popravljen
+function closePlugin() {
+    return {
+        name: 'close-plugin',
+        closeBundle() {
+            console.log('Build completed - forcing exit');
+            process.exit(0);
+        }
+    }
+}
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
-    // vitePrerenderPlugin({
-    //   renderTarget: '#root',
-    //   prerenderScript: path.resolve('./src/main.jsx')
-    // }),
+    vitePrerenderPlugin({
+      renderTarget: '#root',
+      prerenderScript: path.resolve('./src/main.jsx')
+    }),
+    closePlugin(),
   ],
 })
